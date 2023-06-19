a3e5ba6f3176b27ee330f64d2bdb841c71d521f9
JT <joshuasthomas@me.com>
2023-06-19 13:35:25 +0200

    midway thesis update

--- PostcodeInfo/PostCodeExtraInfo.py
+++ PostcodeInfo/PostCodeExtraInfo.py
@@ -14,19 +14,65 @@ urbanity = df['urbanity']
 density = df['density']
 df = df.iloc[:, :-2]
 df = df.replace(-99997, 2)
+df.columns
 
-first_col = df.iloc[:, 0]  # Get the first column
-df_percent = df.iloc[:, 1:].div(first_col, axis=0).mul(100)
+#Gender
+df = df.drop('Vrouw', axis =1)
+df.Man = df.Man.div(df.Totaal, axis =0).mul(100)
 
-# Concatenate the first column with the percentage columns
-df_result = pd.concat([first_col, df_percent], axis=1)
-df_result = df_result.join(density)
+age = ['tot 15 jaar', '15 tot 25 jaar', '25 tot 45 jaar', '45 tot 65 jaar', '65 jaar en ouder']
+df[age] = df[age].div(df.Totaal, axis =0).mul(100)
+pca =  PCA(n_components=2).fit(df[age].values)
+redu = pca.transform(df[age].values)
+df[['Age' + str(x) for x in range(2)]] = redu
+df = df.drop(age, axis = 1)
+
+
+#Househoulds
+hh = ['Eenpersoons', 'Meerpersoons \nzonder kinderen', 'Eenouder', 'Tweeouder', 'Huishoudgrootte']
+df[hh] = df[hh].div(df['Totaal.1'], axis =0).mul(100)
+pca =  PCA(n_components=2).fit(df[hh].values)
+redu = pca.transform(df[hh].values)
+df[['hh' + str(x) for x in range(2)]] = redu
+df = df.drop(hh, axis = 1)
+
+build_age = ['voor 1945','1945 tot 1965', '1965 tot 1975', '1975 tot 1985', '1985 tot 1995', '1995 tot 2005', '2005 tot 2015', '2015 en later']
+df[build_age]  = df[build_age].div(df['Totaal.2'], axis =0).mul(100)
+
+pca =  PCA(n_components=2).fit(df[build_age].values)
+redu = pca.transform(df[build_age].values)
+df[['build_age' + str(x) for x in range(2)]] = redu
+df = df.drop(build_age, axis = 1)
+
+
+Im_Stat = ['Geboren in Nederland met een Nederlandse herkomst',
+       'Geboren in Nederland met een Europese herkomst (excl. Nederland)',
+       'Geboren in Nederland met herkomst buiten Europa',
+       'Geboren buiten Nederland met een Europese herkomst (excl. Nederland)',
+       'Geboren buiten Nederland met een herkomst buiten Europa']
+pca =  PCA(n_components=2).fit(df[Im_Stat].values)
+redu = pca.transform(df[Im_Stat].values)
+df[['Immigration' + str(x) for x in range(2)]] = redu
+df = df.drop(Im_Stat, axis = 1)
+df.columns
+
+df = df.join(density)
+
+size = ['Totaal', 'Totaal.1', 'Totaal.2', 'Meergezins', 'density']
+pca =  PCA(n_components=2).fit(df[size].values)
+redu = pca.transform(df[size].values)
+df[['size' + str(x) for x in range(2)]] = redu
+df = df.drop(size, axis = 1)
+
+df = df.drop('Koopwoning', axis =1)
+df = df.drop(['Huurcoporatie', 'Niet bewoond','WOZ-waarde\nwoning',  'Personen met WW, Bijstand en/of AO uitkering\nBeneden AOW-leeftijd'],axis =1)
+
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 
 # Apply Min-Max scaling to all columns
-df_scaled = pd.DataFrame(scaler.fit_transform(df_result), columns=df_result.columns, index = df_result.index)
+df_scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index = df.index)
 
 
 
@@ -36,9 +82,14 @@ df_scaled = pd.DataFrame(scaler.fit_transform(df_result), columns=df_result.colu
 df_scaled.to_pickle('PostcodeInfo/PCData')
 df_reduced.to_pickle('PostcodeInfo/PCDataReduced')
 
+pd.read_pickle('PostcodeInfo/PCDataReduced').columns
+
+
 df_reduced = df_scaled.copy()
-redu = PCA(n_components=5).fit_transform(df_reduced.values)
-df_reduced[['Emb' + str(x) for x in range(5)]] = redu
+pca =  PCA(n_components=4).fit(df_reduced.values)
+redu = pca.transform(df_reduced.values)
+df_reduced[['Emb' + str(x) for x in range(4)]] = redu
+pcadf = pd.DataFrame(pca.components_, columns = df_scaled.columns).T
 df_reduced = df_reduced.join(urbanity)
 corr = df_reduced.corr()
 
--- VAE/VAE_Analysis.py
+++ VAE/VAE_Analysis.py
@@ -14,18 +14,30 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import json
+with open('Odin/Explanations/otpexpl.json', "r") as json_file:
+ choice_dict = json.load(json_file)
 encoder = LabelEncoder()
-APC = gpd.read_file('PublicGeoJsons/AmsPCs.json')
+APC = gpd.read_file('PublicGeoJsons/AmsterdamPC4.geojson')
 VAE_dimensionality = 10
 n_components=3
-df = pd.read_pickle('VAE/WithoutKHVM/Odin/E10')
+df = pd.read_pickle('VAE/OldNoVerplid/WithoutKHVM/Odin/E10')
 odin2019 = pd.read_pickle('Odin/OdinModellingData/Odin2019All')
+odin2019 = odin2019.sort_index()
+extra = pd.read_pickle('Odin/OldWithVerplid/odin2019sql')
+df.index = odin2019.index
+hvmexpl = choice_dict['hvm']
 df['khvm'] = odin2019['khvm']
+extra.hvm = extra.hvm.astype(int).astype(str)
+extra = extra.replace({"hvm": choice_dict['hvm']})
+extra.hvm = extra.hvm.astype(str)
+df = df.join(extra[['hvm', 'windspeed', 'temp', 'feelslike',
+       'description']], how = 'inner')
 
+#TODO - fix odin columns to be categorical, get 2020 data to match
 
-
-
-#TODO - fix odin columns to be categorical, fix the felyxotp choice dur thing before modelling, fix
+grouped = df.groupby(['khvm','hvm']).count()
+# df = df[df.hvm.isin(list(grouped[grouped.vertpc > 50].reset_index().hvm))]
 
 def ReduceDF(df, reductionmethod = 'PCA', fit = None):
  if fit:
@@ -40,20 +52,25 @@ def ReduceDF(df, reductionmethod = 'PCA', fit = None):
 # sns.scatterplot(data = df.sample(300), x = 'PC0', y = 'PC1', hue = 'khvm')
  return df, pca
 df, pca = ReduceDF(df)
-felyx, _ = ReduceDF(felyx, fit = pca)
+def plotodin(Odin, colorcol='choice', centers=True, filter = False):
+ if filter != False:
+  Odin = Odin[Odin[colorcol].isin(filter)]
 
-def plotodin(Odin, colorcol='choice', centers=True):
  colormap = colormaps.get_cmap('tab20')  # , len(show[colorcol].unique()))
  color_dict = {category: colormap(i) for i, category in enumerate(Odin[colorcol].unique())}
 
  fig = plt.figure(figsize=(8, 6))
  ax = fig.add_subplot(111, projection='3d')
- ax.scatter(*[Odin['PC' +str(x)] for x in range(3)], c=Odin[colorcol].map(color_dict))
+ if type(Odin[colorcol].iloc[0]) == str:
+  p = ax.scatter(*[Odin['PC' +str(x)] for x in range(3)], c=Odin[colorcol].map(color_dict))
 
+  for category, color in color_dict.items():
+   ax.plot([], [], 'o', color=color, label=category[:7])
+   ax.legend()
 
- for category, color in color_dict.items():
-  ax.plot([], [], 'o', color=color, label=category)
-  ax.legend()
+ else:
+  p = ax.scatter(*[Odin['PC' +str(x)] for x in range(3)], c=Odin[colorcol])
+  plt.colorbar(p)
 
  ax.set_xlabel('PC0')
  ax.set_ylabel('PC1')
@@ -61,49 +78,91 @@ def plotodin(Odin, colorcol='choice', centers=True):
  ax.set_title(colorcol)
 
  plt.show()
-# plotshow(df.sample(3000), colorcol = 'khvm')
-plotshow(amsdf.sample(200), colorcol = 'khvm')
+plotodin(df.sample(1000), colorcol = 'khvm')
+# plotodin(df, colorcol = 'hvm', filter = ['Snorfiets', 'Bromfiets', 'Bestuurder'])
+# colorcolview = 'leeftijd'
+# plotodin(df.join(odin2019[colorcolview]).sample(300), colorcol = colorcolview)
+def xgc(df, samples =0, reduce = 0, target = 'hvm', weather = False):
 
-def xgc(df, samples =0, reduce = 0, target = 'khvm'):
+ ledict = dict()
+ for i in ['windspeed', 'temp', 'feelslike', 'description', target]:
+  encoder = LabelEncoder()
+  encoder.fit(df[i])
+  ledict[i] = encoder
+  df[i] = encoder.transform(df[i])
+
  if samples > 0:
   df = df.sample(samples, random_state=42)
- df[target] = encoder.fit_transform(df[target])
 
  model = xgb.XGBClassifier()
- X = df[['emb' + str(x) for x in range(10)]]
+ if weather == True:
+  X = df[['emb' + str(x) for x in range(10)] + ['windspeed', 'temp', 'feelslike','description']]
+ else:
+  X = df[['emb' + str(x) for x in range(10)]]
  if reduce > 0:
   X = df[['PC' + str(x) for x in range(3)]]
  y = df[target]
 
  X_train, X_test, y_train, y_test = train_test_split(
-  X, y, test_size=0.33, random_state=42)
+  X, y, test_size=0.3, random_state=28)
 
  model.fit(X_train, y_train)
  y_pred = model.predict(X_test)
 
- X_test['predictions'] = encoder.inverse_transform(y_pred)
- X_test['khvm'] = encoder.inverse_transform(y_test)
+ for i in ['windspeed', 'temp', 'feelslike', 'description']:
+  df[i] = ledict[i].inverse_transform(df[i])
+  X_test[i] = encoder.inverse_transform(y_test)
+
+ X_test['predictions'] = ledict[target].inverse_transform(y_pred)
+ X_test[target] = ledict[target].inverse_transform(y_test)
 
  print(accuracy_score(y_test, y_pred))
- cm = confusion_matrix(y_test, y_pred)
- types = encoder.inverse_transform(list(range(len(df[target].unique()))))
- print(types)
+ types = encoder.inverse_transform(list(range(len(y_test.unique()))))
+ print(classification_report(X_test.predictions, X_test[target], target_names=types))
+ cm = confusion_matrix(X_test.predictions, X_test[target])
  cmdf = pd.DataFrame(cm, index=types, columns=types)
- print(classification_report(y_test, y_pred, target_names= types))
 
- return X_test, model, encoder, cmdf
+ return X_test, model, cmdf
 
-TestDF, classifier, encoder, cm = xgc(df, 100000)
-TestDF =TestDF.join(df[['aankpc', 'vertpc']])
-amsdf = TestDF[TestDF.aankpc.isin([str(x) for x in APC.Postcode4.unique()]) & TestDF.vertpc.isin([str(x) for x in APC.Postcode4.unique()])]
+TestDF, classifier, confus = xgc(df, target = 'hvm')
+TestDF2, classifier, confus2 = xgc(df, target = 'khvm', weather = True)
+
+
+cm2 = confusion_matrix(TestDF2.predictions, TestDF2.khvm)
+cmdf2 = pd.DataFrame(cm2, index = types2, columns = types2)
+print(classification_report(TestDF2.predictions, TestDF2.khvm, target_names= types2))
+
+def amsify(tdf):
+ tdf = tdf.join(df[['aankpc', 'vertpc']])
+ amsdf = tdf[tdf.aankpc.isin([str(x) for x in APC.Postcode4.unique()]) & tdf.vertpc.isin([str(x) for x in APC.Postcode4.unique()])]
+ return amsdf
+
+aall = amsify(df.drop(['aankpc', 'vertpc'], axis = 1))
+aall.khvm.value_counts()
+a2 = amsify(TestDF2)
+a1 = amsify(TestDF)
+def makecm(df, typ):
+ cm = confusion_matrix(df.predictions, df.khvm)
+ cmdf = pd.DataFrame(cm, index=typ, columns=typ)
+ return cmdf
+
+cc2 = makecm(a2, types2)
+cc = makecm(a1, types)
+
+
+
+
+
 amsdf, _ = ReduceDF(amsdf, fit = pca)
-amsclf, amsenc, amscm = xgc(amsdf)
+# _, amsclf, amsenc, amscm = xgc(amsdf)
+plotodin(amsdf, colorcol = 'khvm')
 
-
-felyx = pd.read_pickle('VAE/WithoutKHVM/Felyx/E10')
+#Felyx
+felyx = pd.read_pickle('VAE/OldNoVerplid/WithoutKHVM/Felyx/E10')
 preds = classifier.predict(felyx[['emb' + str(x) for x in range(10)]])
 felyx['khvm'] = encoder.inverse_transform(preds)
 felyx['khvm'].value_counts()
+felyx, _ = ReduceDF(felyx, fit = pca)
 
 def plotboth(felyx, Odin, colorcol = 'khvm'):
  colormap = colormaps.get_cmap('tab20')  # , len(show[colorcol].unique()))
@@ -126,12 +185,12 @@ def plotboth(felyx, Odin, colorcol = 'khvm'):
  ax.set_zlabel('PC2')
  ax.set_title(colorcol)
  return
-plotboth(felyx.sample(1000), amsdf.sample(1000))
+plotboth(felyx.sample(1000), amsdf)
 plotboth(felyx[felyx.khvm ==  'Te voet'] ,amsdf[amsdf.khvm ==  'Te voet'] )
 
 
 odin2019.doel = odin2019.doel.astype(str)
-classifier, encoder, cm = xgc(df.join(odin2019.doel), 5000, target = 'doel')
+_, classifier, encoder, cm = xgc(df.join(odin2019.doel), 5000, target = 'doel')
 
 
 
